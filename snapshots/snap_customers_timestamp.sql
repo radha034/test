2fbe96f1a02a813ff393aaa158ed3462d341e43b
Radha Patidar <radhabrijpatidar1nov@gmail.com>
2025-07-10 17:40:48 +0000

    c

--- snapshots/snap_customers_timestamp.sql
+++ snapshots/snap_customers_timestamp.sql
@@ -1,10 +1,11 @@
 {% snapshot snap_customers_timestamp %}
 {{config(
-strategy = 'timestamp',
-unique_id = 'id',
-updated_at = 'updated_at'
+  strategy='timestamp',
+  unique_key='id',
+  updated_at= 'updated_at',
+  invalidate_hard_deletes=True
 
-)}}
+  )
+}}
 select * from {{source("Snowflake_Date",'CUSTOMERS')}}
-
 {% endsnapshot %}
